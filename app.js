440812ad14238eab64ce0789c7559eed35fa4675
MhkMafia <mhk1mafia123@gmail.com>
2025-01-05 18:58:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const db = require('./config');
 const authRoutes = require('./routes/auth');
 const busRoutes = require('./routes/buses');
 const bookingRoutes = require('./routes/bookings');
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/buses', busRoutes);
 app.use('/api/bookings', bookingRoutes);
 
-// Start Server
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
