06e896a5da4806bc29944f2c2e739257bfba431c
MhkMafia <mhk1mafia123@gmail.com>
2025-01-05 19:00:22 +0530

    Update buses.js

--- buses.js
+++ buses.js
@@ -3,17 +3,32 @@ const db = require('../config');
 
 const router = express.Router();
 
-// Fetch Available Buses
+// Fetch Buses with Filters
 router.get('/', (req, res) => {
-    const { from, to, date } = req.query;
+    const { from, to, date, type } = req.query;
 
-    const query = `
-        SELECT * FROM Buses
-        WHERE from_city = ? AND to_city = ? AND journey_date = ?
-    `;
-    db.query(query, [from, to, date], (err, results) => {
+    let query = `SELECT * FROM Buses WHERE from_city = ? AND to_city = ? AND journey_date = ?`;
+    const params = [from, to, date];
+
+    if (type && type !== 'all') {
+        query += ` AND type = ?`;
+        params.push(type);
+    }
+
+    db.query(query, params, (err, results) => {
         if (err) return res.status(500).send(err);
         res.json(results);
+    });
+});
+
+// Add New Bus (Admin)
+router.post('/add', (req, res) => {
+    const { name, fromCity, toCity, journeyDate, fare, type } = req.body;
+
+    const query = `INSERT INTO Buses (name, from_city, to_city, journey_date, fare, type) VALUES (?, ?, ?, ?, ?, ?)`;
+    db.query(query, [name, fromCity, toCity, journeyDate, fare, type], (err) => {
+        if (err) return res.status(500).send(err);
+        res.status(201).send('Bus added successfully!');
     });
 });
 
